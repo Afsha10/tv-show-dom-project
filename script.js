ed2c0b79cc82cde3793a19116a9a28a20e400ae8
Afsha10 <115444059+Afsha10@users.noreply.github.com>
2023-04-28 21:30:13 +0100

    Updated index.html and style.css and wrote pseudocode in script.js

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
+  console.log(allEpisodes);
   makePageForEpisodes(allEpisodes);
 }
+
+/*
+Pseudocode:
+1. We are working with an episode list which is an array
+  - create a container using JS connected to "#root"
+  - we would need an H1 tag
+
+2. For getting each episode we need access each element inside the array of objects.
+3. For each movie element we would need to have name of the episodes, series and episode numbers, image of the episode and brief description of the movie
+  - we would need h2, img, p (description) tag for each movie
+
+*/
+
+
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
@@ -10,3 +25,4 @@ function makePageForEpisodes(episodeList) {
 }
 
 window.onload = setup;
+
