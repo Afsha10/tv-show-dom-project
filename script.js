74edfaad8d34f8483abd6e3b13bed981ec2628af
Afsha10 <115444059+Afsha10@users.noreply.github.com>
2023-05-03 04:58:16 +0100

    Clearing unnecessary code

--- script.js
+++ script.js
@@ -76,9 +76,6 @@ function makePageForEpisodes(episodeList) {
 
     const episodeDescriptionTextContainer = document.createElement("div");
     episodeDescriptionTextContainer.classList.add("episode-description-container");
-    episodeInfoCard.appendChild(episodeDescriptionTextContainer);
-
-    // creating a episodeDescriptionTextElement for each episode as the third child of episodeInfoCard. This will hold the episode description.
 
     const episodeDescriptionTextElement = document.createElement("p");
     episodeDescriptionTextElement.classList.add("episode-description");
